0a0e06d54f06808ed92be428f1e77795c4b3ee12
y330 <avivyonah@gmail.com>
2021-07-12 22:11:56 -0400

    updated posts

--- src/components/LatestPosts.svelte
+++ src/components/LatestPosts.svelte
@@ -1,52 +1,64 @@
 <script lang="ts">
-
 </script>
 
-            <div class="max-w-5xl px-6 mx-auto text-center">
-                <h2 class="text-2xl font-semibold text-gray-800">Latest Posts</h2>
+<div class="max-w-5xl px-6 mx-auto text-center">
+	<h2 class="text-2xl font-semibold text-gray-800">Latest Posts</h2>
 
-                <div class="flex flex-col items-center justify-center mt-6">
-                    <a class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md" href="#">
-                        <div class="flex items-center justify-between px-4 py-2">
-                            <h3 class="text-lg font-medium text-gray-700">Easy, Free Laravel CI Using GitHub Actions</h3>
-                            <span class="block text-sm font-light text-gray-600">20 Jan 2020</span>
-                        </div>
-                    </a>
+	<div class="flex flex-col items-center justify-center mt-6">
+		<a
+			class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md"
+			href="#"
+		>
+			<div class="flex items-center justify-between px-4 py-2">
+				<h3 class="text-lg font-medium text-gray-700">
+					Pydash - Full-blown clone of Geometry Dash implemented in
+					Python (Pygame).
+				</h3>
+				<span class="block text-sm font-light text-gray-600"
+					>Nov-2020 - Dec-2020</span
+				>
+			</div>
+		</a>
 
-                    <a class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl mt-8 transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md" href="#">
-                        <div class="flex items-center justify-between px-4 py-2">
-                            <h3 class="text-lg font-medium text-gray-700">Pest: a delightful PHP Testing Framework</h3>
-                            <span class="block text-sm font-light text-gray-600">29 Oct 2019</span>
-                        </div>
-                    </a>
+		<a
+			class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl mt-8 transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md"
+			href="#"
+		>
+			<div class="flex items-center justify-between px-4 py-2">
+				<h3 class="text-lg font-medium text-gray-700">
+					FragmentQR: A chrome extension that copies links that
+					highlight relevant text when opened
+				</h3>
+				<span class="block text-sm font-light text-gray-600"
+					>Aug-2020 - Present</span
+				>
+			</div>
+		</a>
+	</div>
 
-                    <a class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl mt-8 transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md" href="#">
-                        <div class="flex items-center justify-between px-4 py-2">
-                            <h3 class="text-lg font-medium text-gray-700">Using inline SVGs in Vue components</h3>
-                            <span class="block text-sm font-light text-gray-600">15 Oct 2019</span>
-                        </div>
-                    </a>
+	<div class="flex items-center justify-center mt-12">
+		<a
+			class="hover:underline hover:text-gray-500 flex items-center text-gray-600"
+			href="#"
+		>
+			<span>View More</span>
 
-                    <a class="hover:-translate-y-1 hover:scale-110 block w-full max-w-2xl mt-8 transition duration-500 ease-in-out transform bg-white border-t-4 border-indigo-600 rounded-md shadow-md" href="#">
-                        <div class="flex items-center justify-between px-4 py-2">
-                            <h3 class="text-lg font-medium text-gray-700">Acceptance Testing Laravel & VueJs Apps with Codeception</h3>
-                            <span class="block text-sm font-light text-gray-600">3 Oct 2019</span>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="flex items-center justify-center mt-12">
-                    <a class="hover:underline hover:text-gray-500 flex items-center text-gray-600" href="#">
-                        <span>View More</span>
-
-                        <svg class="w-5 h-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>
-                        </svg>
-                    </a>
-                </div>
-            </div>
-
+			<svg
+				class="w-5 h-5 ml-1"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M17 8l4 4m0 0l-4 4m4-4H3"
+				/>
+			</svg>
+		</a>
+	</div>
+</div>
 
 <style>
 </style>
-
